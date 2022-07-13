ec0758ea932b96467fd4fee2e5c1ffc8504581de
Alejo Boga <alejoboga20@gmail.com>
2022-07-13 10:58:03 -0300

    Pokemon slice

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import { store } from './store';
-import { App } from './App';
+import { PokemonApp } from './store/PokemonApp';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<App />
+			<PokemonApp />
 		</Provider>
 	</React.StrictMode>
 );
---
+++ src/store/PokemonApp.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const PokemonApp = () => {
+	return (
+		<>
+			<h1>Pokemon App</h1>
+			<hr />
+		</>
+	);
+};
